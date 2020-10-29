a12091505182b868c7674569ef93ed8ea12c60b0
Guilherme Paixão <guilherme.paixao91@yahoo.com.br>
2020-10-29 19:20:06 -0300

    Commit inicial

---
+++ src/main/java/com/elo7/sondadeexploracao/SondaDeExploracaoApplication.java
@@ -0,0 +1,13 @@
+package com.elo7.sondadeexploracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SondaDeExploracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SondaDeExploracaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elo7/sondadeexploracao/SondaDeExploracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elo7.sondadeexploracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SondaDeExploracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
